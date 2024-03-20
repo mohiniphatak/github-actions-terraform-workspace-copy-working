50b725747bd303ded4d1267ace6b0f6b10fbfb29
phatakmohini@gmail.com <phatakmohini@gmal.com>
2024-03-20 13:41:16 +0530

    bucket and backend changed

--- aws_s3_bucket/main.tf
+++ aws_s3_bucket/main.tf
@@ -7,7 +7,7 @@ terraform {
 }
 
 resource "aws_s3_bucket" "test" {
-  bucket = "${var.environment}.some-shitty-bucket-name-qunitw"
+  bucket = "some-shitty-bucket-name-qunitw"
   # bucket    = "${var.environment}.my-bucket-yeeeeeeeeeee"
 }
 
